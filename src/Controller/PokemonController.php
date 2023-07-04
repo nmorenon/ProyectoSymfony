ca72798dde219e45714ff3eda20738310dd1c4a5
Manuhergreen <manuhernandezbi@gmail.com>
2023-07-04 10:27:31 +0200

    agrego lista pokemon

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -7,7 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PokemonController extends AbstractController
 { 
-    #[Route("/pokemon")]
+    #[Route("/pokemon", name:"getPokemon")]
     public function getPokemon()
     {
         $pokemon=[
@@ -19,5 +19,30 @@ class PokemonController extends AbstractController
 
         return $this -> render("Pokemons/GetPokemon.html.twig", ["pokemon" => $pokemon] );
 
+    }
+    #[Route("/listaPokemon", name: "listaPokemon")]
+    public function listaPokemons()
+    {
+        $listaPokemon=[
+            [
+            "nombre"=>"pikachu",
+            "descripcion"=>"amarillo y gordito",
+            "foto"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
+            "codigo"=>"#0025"
+            ],
+            ["nombre"=>"doraimon",
+            "descripcion"=>"amigo del gordito",
+            "foto"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/030.png",
+            "codigo"=>"#0030"],
+            ["nombre"=>"filemon",
+            "descripcion"=>"amarillo y gordito",
+            "foto"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/031.png",
+            "codigo"=>"#0025"],
+            ["nombre"=>"mortadelo",
+            "descripcion"=>"amarillo y gordito",
+            "foto"=>"https://assets.pokemon.com/assets/cms2/img/pokedex/full/032.png",
+            "codigo"=>"#0032"],
+        ];
+        return $this->render ("Pokemons/ListPokemon.html.twig", ["listPokemon" => $listaPokemon] );
     }
 }
