9def67fcafcbe035ced96778759c1faf961f32d3
nmorenon <nmorenonunez@gmail.com>
2023-07-04 15:08:40 +0200

    consultas db

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -38,47 +38,19 @@ class PokemonController extends AbstractController
         $doctrine->flush();
         return new Response("pokemon insertado correctamente");
     }
-    #[Route("/pokemon", name: "getPokemon")]
-    public function getPokemon()
+    #[Route("/pokemon/{id}", name: "getPokemon")]
+    public function getPokemon(EntityManagerInterface $doctrine, $id)
     {
-        $pokemon = [
-            "nombre" => "pikachu",
-            "descripcion" => "amarillo y gordito",
-            "foto" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-            "codigo" => "#0025"
-        ];
-
+        $repo = $doctrine->getRepository(Pokemon::class);
+        $pokemon = $repo-> find($id);
+       # dd($pokemon);
         return $this->render("Pokemons/GetPokemon.html.twig", ["pokemon" => $pokemon]);
     }
     #[Route("/listaPokemon", name: "listaPokemon")]
-    public function listaPokemons()
+    public function listaPokemons(EntityManagerInterface $doctrine)
     {
-        $listaPokemon = [
-            [
-                "nombre" => "pikachu",
-                "descripcion" => "amarillo y gordito",
-                "foto" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-                "codigo" => "#0025"
-            ],
-            [
-                "nombre" => "doraimon",
-                "descripcion" => "amigo del gordito",
-                "foto" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/030.png",
-                "codigo" => "#0030"
-            ],
-            [
-                "nombre" => "filemon",
-                "descripcion" => "amarillo y gordito",
-                "foto" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/031.png",
-                "codigo" => "#0025"
-            ],
-            [
-                "nombre" => "mortadelo",
-                "descripcion" => "amarillo y gordito",
-                "foto" => "https://assets.pokemon.com/assets/cms2/img/pokedex/full/032.png",
-                "codigo" => "#0032"
-            ],
-        ];
+        $repo = $doctrine->getRepository(Pokemon::class);
+        $listaPokemon = $repo-> findAll();
         return $this->render("Pokemons/ListPokemon.html.twig", ["listPokemon" => $listaPokemon]);
     }
 }
